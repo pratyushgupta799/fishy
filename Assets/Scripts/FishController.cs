7d3a2ce79c670065b19d9690d0e80f2abeb504c4
Pratyush-bruce-Gupta <pratyushgupta04@gmail.com>
2025-10-28 18:13:12 +0530

    controller working

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class FishController : MonoBehaviour
 {
-    private Rigidbody rb;
     [SerializeField] private GameObject camera;
     private Transform camForward;
     
@@ -12,11 +11,13 @@ public class FishController : MonoBehaviour
     
     [SerializeField] private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
+    
+    [SerializeField] private CharacterController characterController;
 
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -26,45 +27,18 @@ public class FishController : MonoBehaviour
         {
             MoveCharacter();
         }
-        else
-        {
-            DisableMovement();
-            Debug.Log("yo");
-        }
     }
 
     private void MoveCharacter()
     {
-        Vector3 cameraForward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z).normalized;
-        transform.rotation = Quaternion.LookRotation(cameraForward);
-        // transform.Rotate(new Vector3(0, 0, 0), Space.Self);
+        Vector3 cameraForward = camera.transform.forward;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(cameraForward),
+            turnSmoothTime * Time.deltaTime);
         
         Vector3 forward = camera.transform.forward;
         Vector3 swimDirection = forward.normalized;
         
-        rb.AddForce(swimDirection * speed, ForceMode.VelocityChange);
+        characterController.Move(swimDirection * speed * Time.deltaTime);
         
     }
-
-    private void DisableMovement()
-    {
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
-    }
-
-    // private void FixedUpdate()
-    // {
-    //     float horizontal = Input.GetAxisRaw("Horizontal");
-    //     float vertical = Input.GetAxisRaw("Vertical");
-    //     Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-    //     
-    //     if (direction.magnitude >= 0.1f)
-    //     {
-    //         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.transform.eulerAngles.y;
-    //         
-    //         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(camera.transform.eulerAngles.x, targetAngle, 0), turnSmoothTime);
-    //
-    //         Vector3 moveDirection = Quaternion.Euler(camera.transform.eulerAngles.x, targetAngle, 0f) * Vector3.forward;
-    //         rb.AddForce(moveDirection.normalized * speed * Time.deltaTime, ForceMode.VelocityChange);
-    //     }
-    // }
 }
