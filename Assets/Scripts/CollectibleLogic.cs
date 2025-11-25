8453900674b1d072b6ea2668afcd5c72fbb90fe6
jaspreet3d <matharoo3d@gmail.com>
2025-11-25 17:28:33 +0530

    duckrotation and some level1 changes

--- Assets/Scripts/CollectibleLogic.cs
+++ Assets/Scripts/CollectibleLogic.cs
@@ -10,7 +10,10 @@ public class CollectibleLogic : MonoBehaviour
     [SerializeField] private float yMagnitude = 0.5f;
     [SerializeField] private float yFrequency = 1f;
 
-    private Vector3 initialScale;
+	[Header("Y Rotation Spin")]
+	[SerializeField] private float rotationSpeed = 1f;
+
+	private Vector3 initialScale;
     private Vector3 initialPosition;
 
     void Start()
@@ -28,6 +31,9 @@ public class CollectibleLogic : MonoBehaviour
         // Y position pulse
         float yPulse = Mathf.Sin(Time.time * yFrequency * 2f * Mathf.PI) * yMagnitude;
         transform.position = initialPosition + Vector3.up * yPulse;
+
+        // Y Rotation spin
+        transform.Rotate(0, 1 * rotationSpeed, 0);
     }
 
     void OnTriggerEnter(Collider other)
