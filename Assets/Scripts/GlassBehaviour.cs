5ede03b5ec2df59c2bf8ac5a303b9ef8f86ffef8
Pratyush-bruce-Gupta <pratyushgupta04@gmail.com>
2025-11-28 12:13:50 +0530

    checkpoint and death

--- Assets/Scripts/GlassBehaviour.cs
+++ Assets/Scripts/GlassBehaviour.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
 
-public class GlassBehaviour : MonoBehaviour
+public class GlassBehaviour : MonoBehaviour, IInteractible
 {
+    private bool isDirty = false;
+    
     private Rigidbody rb;
 
     [SerializeField] private GameObject waterMesh;
     [SerializeField] private GameObject puddleMesh;
     [SerializeField] private GameObject puddleCenter;
     [SerializeField] private GameObject surfaceMesh;
+
+    private GameObject droppedPuddle;
     
     [SerializeField, Range(0f, 180f)] private float minTiltToSpill = 70f;
 
+    [SerializeField] private int checkpointIndex = 1;
+
     private bool hasSpilled;
+
+    private Vector3 defaultPos;
+    private Quaternion defaultRot;
+    
+    public int CheckpointIndex => checkpointIndex;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        defaultPos = this.transform.position;
+        defaultRot = this.transform.rotation;
     }
 
     // Update is called once per frame
@@ -43,15 +56,46 @@ public class GlassBehaviour : MonoBehaviour
         Ray ray = new Ray(puddleCenter.transform.position, Vector3.down);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Instantiate(puddleMesh, hit.point, Quaternion.identity);
-            waterMesh.GetComponent<Collider>().enabled = false;
-            surfaceMesh.GetComponent<Collider>().enabled = false;
+            droppedPuddle = Instantiate(puddleMesh, hit.point, Quaternion.identity);
             waterMesh.SetActive(false);
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SetDiry(CheckPointManager.Instance.CurrentCheckpointIndex);
+        }
+    }
+
+    public void RestoreState()
+    {
+        if (CheckPointManager.Instance.CurrentCheckpointIndex <= checkpointIndex)
+        {
+            transform.position = defaultPos;
+            transform.rotation = defaultRot;
+
+            if (droppedPuddle != null)
+            {
+                Destroy(droppedPuddle.gameObject);
+            }
+
+            waterMesh.SetActive(true);
+            hasSpilled = false;
+        }
+    }
+
+    public void ApplyDirty()
+    {
+        isDirty = false;
+    }
+    
+    public void SetDiry(int index)
+    {
+        if (isDirty) return;
+        isDirty = true;
         
+        CheckPointManager.Instance.AddChangedPrefab(gameObject);
     }
 }
