7c309d443b36073e0cc1724bd5095ac91e88b207
Pratyush-bruce-Gupta <pratyushgupta04@gmail.com>
2025-12-17 12:17:04 +0530

    random bugs gooo

---
+++ Assets/Scripts/UI/ButtonBehaviour.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private RectTransform rect;
+    private Vector3 startPos;
+    private Vector3 startScale;
+    
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        startPos = transform.localPosition;
+        startScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rect.DOAnchorPosY(startPos.y + 10f, 0.2f).SetEase(Ease.OutBack);
+        rect.DOScale(startScale * 1.1f, 0.2f).SetEase(Ease.OutBack);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rect.DOAnchorPosY(startPos.y, 0.2f).SetEase(Ease.OutBack);
+        rect.DOScale(startScale, 0.2f).SetEase(Ease.OutBack);
+    }
+}
