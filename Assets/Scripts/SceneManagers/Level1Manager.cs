b90486792dc997a1e1cf328f42f770861b94b4dc
Pratyush-bruce-Gupta <pratyushgupta04@gmail.com>
2026-01-27 22:48:17 +0530

    i did some stuff and now controller not work

--- Assets/Scripts/SceneManagers/Level1Manager.cs
+++ Assets/Scripts/SceneManagers/Level1Manager.cs
@@ -1,10 +1,27 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Level1Manager : MonoBehaviour
 {
     [SerializeField] private Transform fishy;
-    [SerializeField] private GameObject planeOfDeath;
+    
+    [SerializeField] private PlayerInput playerInput;
+    
+    [SerializeField] private List<TextMeshPro> keyboardTexts;
+    [SerializeField] private List<TextMeshPro> gamepadTexts;
+
+    void OnEnable()
+    {
+        playerInput.onControlsChanged += OnControlsChanged;
+    }
+
+    void OnDisable()
+    {
+        playerInput.onControlsChanged -= OnControlsChanged;
+    }
     
     void Start()
     {
@@ -17,5 +34,35 @@ public class Level1Manager : MonoBehaviour
         
         Vector2 fishyPos = Camera.main.WorldToScreenPoint(fishy.position);
         await CircleTransition.Instance.CircleOut(fishyPos);
+    }
+    
+    void OnControlsChanged(PlayerInput pi)
+    {
+        if (pi.currentControlScheme == "KeyboardMouse")
+        {
+            SetKeyboardTextsActive(true);
+            SetControllerTextsActive(false);
+        }
+        else
+        {
+            SetKeyboardTextsActive(false);
+            SetControllerTextsActive(true);
+        }
+    }
+
+    void SetKeyboardTextsActive(bool active)
+    {
+        for (int i = 0; i < keyboardTexts.Count; i++)
+        {
+            keyboardTexts[i].gameObject.SetActive(active);
+        }
+    }
+    
+    void SetControllerTextsActive(bool active)
+    {
+        for (int i = 0; i < gamepadTexts.Count; i++)
+        {
+            gamepadTexts[i].gameObject.SetActive(active);
+        }
     }
 }
